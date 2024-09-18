e0bdcdbaf33556c1a6c06252d149db49b6ad52e8
rahulll18 <rahulavhad005@student.sfit.ac.in>
2024-09-18 10:13:38 +0530

    routing and navigation added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminLoginComponent } from './admin-login/admin-login.component';
+import { ProductsContainerComponent } from './products-container/products-container.component';
+import { ProductFormComponent } from './product-form/product-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'adminlogin',
+    pathMatch: 'full',
+  },
+  {
+    path: 'adminlogin',
+    component: AdminLoginComponent,
+  },
+  {
+    path: 'products',
+    component: ProductsContainerComponent,
+  },
+  {
+    path: 'addProduct',
+    component: ProductFormComponent,
+  },
+  {
+    path: 'editProduct/:productId',
+    component: ProductFormComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}

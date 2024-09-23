da252253a85122d5c2fa033d260134a597f0f01e
rahulll18 <rahulavhad005@student.sfit.ac.in>
2024-09-23 16:04:23 +0530

    cookie added

--- src/app/guards/authGard.ts
+++ src/app/guards/authGard.ts
@@ -1,11 +1,13 @@
 import { inject } from '@angular/core';
-import { UseraccountService } from '../customservice/useraccount.service';
+
+import { CookieService } from 'ngx-cookie-service';
 
 export function authGuard(): boolean {
   //if we want to inject service in class then : DI : constructor
   // if we want to inject the service in function then following
-  const account = inject(UseraccountService);
-  if (account.loginFlag) return true;
+  const cookie = inject(CookieService);
+  const username = cookie.get('user');
+  if (username != '') return true;
   else {
     window.alert('Please login first....');
     // navigate to AdminLogin component
